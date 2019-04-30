ee299243cae96927267fc90c67e4c744e2379436
Noto <akbar.exp57@gmail.com>
2019-05-01 05:43:16 +0700

    search sampun

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,7 +1,7 @@
 @extends('template.index')
 @section('main')
-
-    <div class="filter-wrapper">
+<div class="row">
+    <div class="filter-wrapper col-md-4">
         
             <div class="filter-header">
                 <h4 id="filter-close-title" class="filter-header-title">Filter</h4>
@@ -18,14 +18,14 @@
             <div id="filter">
             <div class="filter-content">
                 <div class="content-of-filters row">
-                        <h5><span>Tipe</span></h5>
+                        <h5><span>Content Type</span></h5>
                         <ul style="padding-left:0">
                             <li class="col-md-5 filter-option left"><span>Video</span></li>
                             <li class="col-md-5 filter-option right"><span>Artikel</span></li>
                         </ul>
                     </div>
                 <div class="content-of-filters row video">
-                    <h5><span>Kualitas</span></h5>
+                    <h5><span>Video Quality</span></h5>
                     <ul style="padding-left:0">
                         <li class="col-md-5 filter-option left"><span>>360P</span></li>
                         <li class="col-md-5 filter-option  right"><span>360P</span></li>
@@ -34,7 +34,7 @@
                     </ul>
                 </div>
                 <div class="content-of-filters row video">
-                    <h5><span>Durasi (menit)</span></h5>
+                    <h5><span>Duration (minutes)</span></h5>
                     <ul style="padding-left:0">
                         <li class="col-md-5 filter-option left"><span>>5</span></li>
                         <li class="col-md-5 filter-option right"><span>5-10</span></li>
@@ -44,6 +44,8 @@
                 <div class="content-of-filters row tag">
                     <h5 class="tag-title"><span>Tag</span></h5>
                         <select class="chosen-select" multiple="multiple">
+                            <option value="">Small Room</option>
+                            <option value="">Wardrobe</option>
                             <option value="">Ruang Sempit</option>
                             <option value="">Lemari</option>
                             <option value="">Ruang luas</option>
@@ -55,11 +57,69 @@
             </div>
         </div>
     </div>
-    <div class="content-wrapper">
-        <div id="content">
-
+    <div class="content-wrapper col-md-9">
+        <div id="content" class="row">
+            <div class="col-md-10 content-container" >
+                <div class="col-md-5 content-tumbnail">
+                    <div class="content-img-container">
+                        <img src="{{ asset('img/tidy-house-2.jpg') }}" width="100%" max-width="288" height="192">
+                    </div>
+                </div>
+                <div class="col-md-7 content-desc">
+                    <div class="content-desc-container col-md-12">
+                        <div class="content-desc-title">
+                            <p class="content-type"><b>VIDEO</b></p>
+                            <h3>How to tidy up your bedroom in 5 mins</h3>
+                            <p class="content-date">04.32 Minutes - 1M Views - 3 Days Ago</p>
+                        </div>
+                        <div class="content-description">
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim...
+                        </div>  
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-10 content-container" >
+                    <div class="col-md-5 content-tumbnail">
+                        <div class="content-img-container">
+                            <img src="{{ asset('img/tidy-house.jpg') }}" width="100%" max-width="288" height="192">
+                        </div>
+                    </div>
+                    <div class="col-md-7 content-desc">
+                        <div class="content-desc-container col-md-12">
+                            <div class="content-desc-title">
+                                <p class="content-type"><b>ARTICLE</b></p>
+                                <h3>How to tidy up your bedroom in 5 mins</h3>
+                                <p class="content-date">1M Clicks - 3 Days Ago</p>
+                            </div>
+                            <div class="content-description">
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim...
+                            </div>  
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-10 content-container" >
+                        <div class="col-md-5 content-tumbnail">
+                            <div class="content-img-container">
+                                <img src="{{ asset('img/tidy-house-2.jpg') }}" width="100%" max-width="288" height="192">
+                            </div>
+                        </div>
+                        <div class="col-md-7 content-desc">
+                            <div class="content-desc-container col-md-12">
+                                <div class="content-desc-title">
+                                    <p class="content-type"><b>VIDEO</b></p>
+                                    <h3>How to tidy up your bedroom in 5 mins</h3>
+                                    <p class="content-date">04.32 Minutes - 1M Views - 3 Days Ago</p>
+                                </div>
+                                <div class="content-description">
+                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim...
+                                </div>  
+                            </div>
+                        </div>
+                    </div>       
         </div>
     </div>
+
+</div>
 
 @endsection
 
@@ -154,6 +214,77 @@
         }
 
     </style>
+
+
+    <style>
+
+        .content-wrapper{
+            /* background-color:white; */
+            height: 100vh;
+            margin-left: 2%;
+            margin-top:20px;
+        }
+
+        .content-container{
+            height:33vh;
+            cursor: pointer;
+            border-radius: 5px
+        }
+
+
+        .content-img-container{
+            padding-top:5px;
+            padding-bottom:5px;
+            padding-left:0;
+            margin-left:0;
+        }
+
+
+        .content-desc-container{
+            text-overflow: ellipsis;
+        }
+        .content-desc-title h3{
+            margin-top:0;
+            margin-bottom: 0;
+        }
+
+        .content-desc{
+            color:white;
+        }
+
+        .content-desc-title{  
+            white-space: unset;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            padding-left: 0;
+        }
+
+        .content-date{
+            margin-top:0;
+        }
+
+        .content-type{
+            background-color:white;
+            color:black;
+            display: inline-block;
+            padding:1px;
+            padding-left:3px;
+            padding-right:3px;
+            margin-bottom: 0;
+            margin-top:5px;
+        }
+
+        .content-description{
+            white-space: unset;
+            text-overflow: ellipsis;
+            overflow: hidden;
+        }
+
+        .kotakin{
+            border:1px solid white;
+        }
+
+    </style>
 @endsection
 
 @section('additional-scripts')
@@ -186,5 +317,12 @@
 
          });
 
+         $(".content-container").hover(function(){
+            $(this).addClass("kotakin")
+         });
+
+         $(".content-container").mouseleave(function(){
+            $(this).removeClass("kotakin")
+         });
     </script>
 @endsection
