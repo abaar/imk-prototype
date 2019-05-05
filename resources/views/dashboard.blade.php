21d4c16177f14d50bc8a5080894dd364ae11fd48
Jason Wilyandi <jasonwilyandi@yahoo.co.id>
2019-05-05 10:42:17 +0700

    add dashboard.blade.php

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('template.index')
+@section('main')
+    <h1>Dashboard</h1>
+@endsection
+
+@section('additional-styles')
+    <style>
+    </style>
+@endsection
+
+@section('additional-scripts')
+    <script>
+    </script>
+@endsection
