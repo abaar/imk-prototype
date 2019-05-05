1de7bb8e061169a9fd79861afae22f2fc9c70b34
Jason Wilyandi <jasonwilyandi@yahoo.co.id>
2019-05-06 00:42:26 +0700

    add dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,91 @@
 @extends('template.index')
 @section('main')
-    <h1>Dashboard</h1>
+<div class="row">
+    <div class="container col-md-6">
+        <div class="container-homepage">
+            <div class="container-title">
+                <h3>Welcome To</h3>
+
+                <h1>Tidiup</h1>
+                <div class="second-title"><h3>Hahahahahaha</h3></div>
+
+                <div class="text-list-images"><h3>Ini gan</h3></div>
+            </div>
+            <div class="container-image">
+                <div class="container-image-row">
+                    <img class="img" src="{{asset('img/tidy-shoes.jpg')}}" style="width:100%; max-height: 150px; height:100%">
+                </div>
+                <div class="container-image-row">
+                        <img class="img" src="{{asset('img/tidy-wardrobe.jpg')}}" style="width:100%; max-height:150px; height:100%">
+                </div>
+                <div class="container-image-row">
+                        <img class="img" src="{{asset('img/tidy-wardrobe.jpeg')}}" style="width:100%; max-height:150px; height:100%">
+                </div>
+            
+            </div>
+
+        </div>
+    </div>
+    <div class="container col-md-6">
+        <div class="container-homepage">
+            <div class="container-image">
+                <img class="img" src="{{asset('img/tidy-house.jpg')}}" style="width:100%; max-height:375px; height:100%">
+            </div>
+            <div class="container-title">
+                <h2>Hohohihi</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
 
 @section('additional-styles')
     <style>
+    .container-homepage {
+        padding-left: 40px;
+        padding-top: 60px;
+        padding-right: 40px;
+    }
+
+    .container-title {
+        color: black;
+    }
+
+    .container-title h1 {
+        font-size: 100px;   
+    }
+
+    .container-title h2 {
+        text-align: center;
+        font-size: 60px;
+        padding-top: 10px;
+    }
+
+    .second-title {
+        padding-left: 100px;
+    }
+
+    .container-image {
+        display: flex;
+    }
+
+    .container-image-row {
+        flex: 33.33%;
+        padding: 10px;
+    }
+
+    .text-list-images {
+        padding-top: 20px;
+    }
+
+    .img {
+        border-radius: 10px;
+        -webkit-border-radius: 10px;
+        -moz-border-radius: 10px;
+    }
+
+
     </style>
 @endsection
 
--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -1,21 +1,15 @@
 @extends('template.index')
 @section('main')
 
-<div class="content-wrapper col-md-9">
-    <div id="content" class="row">
-        <div class="content-container" >
-            <div class="col-md-5 content-tumbnail">
-                <div class="content-img-container">
-                    <img src="{{ asset('img/tidy-house-2.jpg') }}" width="675px",height="380px">
-                </div>
-                
-                <div class="content-desc-title">
-                    <h1>
-                        Tes123
-                    </h1>
-                </div>
-            </div>
-        </div>
+<div>
+    <div class="vid-container">
+        <img src="{{ asset('img/tidy-house-2.jpg') }}" width="675px",height="380px">
+    </div>
+    <div>
+        <h1>123</h1>
+    </div>
+    <div>
+        <p>HHHHHH</p>
     </div>
 </div>
 
@@ -24,42 +18,17 @@
 
 @section('additional-styles')
     <style>
-        .content-wrapper{
-            /* background-color:white; */
-            height: 100vh;
-            margin-left: 2%;
-            margin-top:20px;
-        }
-
-        .content-container{
-            height:33vh;
-            cursor: pointer;
-            border-radius: 5px
-        }
-
-
-        .content-img-container{
-            padding-top:5px;
-            padding-bottom:5px;
-            padding-left:0;
-            margin-left:0;
-        }
-
-        .content-desc-title h1{
-            margin-top:0;
-            margin-bottom: 0;
-        }
-
-        .content-desc{
-            color:white;
-        }
-
-        .content-desc-title{  
-            white-space: unset;
-            text-overflow: ellipsis;
-            overflow: hidden;
-            padding-left: 0;
-        }
+    .vid-container {
+        padding-top:5px;
+        padding-bottom:5px;
+        padding-left:0;
+        margin-left:0;
+    }
+    .content-wrapper{
+        height: 100vh;
+        margin-left: 1%;
+        margin-top:20px;
+    }
     </style>
 @endsection
 
